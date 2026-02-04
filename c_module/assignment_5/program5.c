ae9a8eab7f5760e7b5ee475f783a96479b13a5f4
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-04 17:54:20 +0530

    added from program  from 5 to 10

---
+++ c_module/assignment_5/program5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main()
+{
+    int n = 5;
+    int i, j;
+
+    for (i = 1; i <= n; i++)
+    {
+        // print spaces
+        for (j = 1; j < i; j++)
+        {
+            printf(" ");
+        }
+
+        // print stars
+        for (j = i; j <= n; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
---
+++ c_module/assignment_5/program8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main()
+{
+    int n = 4;
+    int i, j;
+
+    // Upper half
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Lower half
+    for (i = n - 1; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
