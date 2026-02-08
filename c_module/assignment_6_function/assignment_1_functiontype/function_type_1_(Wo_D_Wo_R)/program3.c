4ad94ade33bff546bc5060a3a8420e412dc8d6a5
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-08 17:48:43 +0530

    added program3

---
+++ c_module/assignment_6_function/assignment_1_functiontype/function_type_1_(Wo_D_Wo_R)/program3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void checkLeap();   // function declaration
+
+void main()
+{
+    checkLeap();    // function call
+}
+
+void checkLeap()    // function definition
+{
+    int year = 2059;
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+    {
+        printf("Leap year");
+    }
+    else
+    {
+        printf("Not Leap year");
+    }
+}
