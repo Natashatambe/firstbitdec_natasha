f0c1aaa8fdb04145548b1384dda76ecaf10b224f
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-20 00:01:32 +0530

    Refactor isperfect function for clarity
    Refactor isperfect function to improve readability.

--- c_module/assignment_6_function/assignment_4_functiontype/function_type_1_(Wo_D_Wo_R)/program3.c
+++ c_module/assignment_6_function/assignment_4_functiontype/function_type_1_(Wo_D_Wo_R)/program3.c
@@ -7,7 +7,7 @@ void main()
 
 
 void isperfect() {
-    int n = 1000;  // You can change this value
+    int n = 1000;  
     int num = 1;
 
     printf("Perfect numbers from 1 to %d are:\n", n);
@@ -16,7 +16,7 @@ void isperfect() {
         int i = 1;
         int sum = 0;
 
-        // Find all divisors of num
+    
         while (i < num) {  // Nested loop
             if (num % i == 0) {
                 sum = sum + i;
@@ -24,7 +24,6 @@ void isperfect() {
             i++;
         }
 
-        // Check if sum of divisors equals the number
         if (sum == num) {
             printf("%d ", num);
         }
