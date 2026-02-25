b720f3e2bc7ff333a6d8c826ff3a7d3ec3c6a235
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 22:50:42 +0530

    Refactor sum function into main
    Removed redundant function call and integrated sum logic directly into main.

--- c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program10.c
+++ c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program10.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 void sum();
 void main()
@@ -22,28 +21,4 @@ void sum() {
     printf("%d", sum);
 }
 
-=======
-#include <stdio.h>
-void sum();
-void main()
-{
-	sum();
-}
 
-void sum() {
-    int n = 12345;       
-    int first, last;
-
-    last = n % 10;      
-
-    int temp = n;
-    while (temp >= 10) { 
-        temp = temp / 10;
-    }
-    first = temp;        
-
-    int sum = first + last;
-    printf("%d", sum);
-}
-
->>>>>>> a6e0a47394198f786ebdc5e4b1e1d7f42da419ee
