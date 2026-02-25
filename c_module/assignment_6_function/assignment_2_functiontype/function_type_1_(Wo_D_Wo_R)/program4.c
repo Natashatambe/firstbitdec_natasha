de6066a2eb467a7bc956b80b2757d78eb3237ca1
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 19:53:08 +0530

    Refactor program4.c by removing duplicate code
    Removed redundant main function and checkMarks function implementation.

--- c_module/assignment_6_function/assignment_2_functiontype/function_type_1_(Wo_D_Wo_R)/program4.c
+++ c_module/assignment_6_function/assignment_2_functiontype/function_type_1_(Wo_D_Wo_R)/program4.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 void checkMarks();
 void main()
@@ -42,49 +41,5 @@ void main()
 		}
 	}
 }
-=======
-#include<stdio.h>
-void checkMarks();
-void main()
-{
-	
-	checkMarks();
-}
-	
-    void checkMarks	(){
-	int marks;
-	printf(" Enter marks:");
-	scanf("%d",&marks);
-	
-	if(marks>75)
-	{
-		printf("Distinction");
-	}
-	else
-	{
-		if(marks>65)	
-		{
-			printf("First Class");
-		}
-		else
-		{
-			if(marks>55)
-			{
-				printf("Second Class");
-			}
-			else
-			{
-				if(marks>=40)
-				{
-					printf("pass class");
-				}
-				else
-				{
-					printf("fail");
-				}
-			}
-		}
-	}
-}
->>>>>>> a6e0a47394198f786ebdc5e4b1e1d7f42da419ee
+
 // This program assigns a grade based on user input for marks
