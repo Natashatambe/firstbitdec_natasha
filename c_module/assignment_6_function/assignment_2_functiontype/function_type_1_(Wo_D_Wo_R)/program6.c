01d3ee82854395a95aa8d8dc1c1db1021878c820
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 22:14:20 +0530

    Refactor program6.c to eliminate redundancy
    Removed duplicate function definition and main function call.

--- c_module/assignment_6_function/assignment_2_functiontype/function_type_1_(Wo_D_Wo_R)/program6.c
+++ c_module/assignment_6_function/assignment_2_functiontype/function_type_1_(Wo_D_Wo_R)/program6.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 void checkDivisiblity();
 void main() 
@@ -26,32 +25,3 @@ void main()
         printf("Divisible by None");
 }
 // This program checks if a user-input number is divisible by 3, 5, both, or neither.
-=======
-#include <stdio.h>
-void checkDivisiblity();
-void main() 
-
-{
-	
-	
-	checkDivisiblity();
-}
-
-
-   void checkDivisiblity(){
-    int num;
-
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    if (num % 3 == 0 && num % 5 == 0)
-        printf("Divisible by both");
-    else if (num % 3 == 0)
-        printf("Divisible by 3 but not by 5");
-    else if (num % 5 == 0)
-        printf("Divisible by 5 but not by 3");
-    else
-        printf("Divisible by None");
-}
-// This program checks if a user-input number is divisible by 3, 5, both, or neither.
->>>>>>> a6e0a47394198f786ebdc5e4b1e1d7f42da419ee
