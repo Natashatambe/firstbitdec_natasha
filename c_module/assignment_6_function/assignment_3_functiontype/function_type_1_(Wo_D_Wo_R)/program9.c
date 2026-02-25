46f7b99b1881c3a6a68def393925fceeac9a3d4a
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 22:54:45 +0530

    Remove duplicate palindrome function implementation

--- c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program9.c
+++ c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program9.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 #include <stdio.h>
 void ispalindrome();
 void main() 
@@ -24,31 +22,4 @@ void ispalindrome(){
         printf("Palindrome");
     else
         printf("Not Palindrome");
-=======
-
-#include <stdio.h>
-void ispalindrome();
-void main() 
-{
-	ispalindrome();
-}
-
-
-void ispalindrome(){
-    int n = 121;          
-    int original = n;     
-    int reversed = 0;     
-    int remainder;
-
-    while (n > 0) {
-        remainder = n % 10;            
-        reversed = reversed * 10 + remainder;  
-        n = n / 10;                   
-    }
-
-    if (reversed == original)
-        printf("Palindrome");
-    else
-        printf("Not Palindrome");
->>>>>>> a6e0a47394198f786ebdc5e4b1e1d7f42da419ee
 }
