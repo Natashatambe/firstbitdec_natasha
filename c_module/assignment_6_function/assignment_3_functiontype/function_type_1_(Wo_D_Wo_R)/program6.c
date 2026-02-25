8498ebd828c34c9a4fb052c16f2199110a9eda0e
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 22:52:55 +0530

    Refactor perfect number check by removing duplicates
    Removed duplicate main function and isperfect function implementation. Simplified the program to check for a perfect number.

--- c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program6.c
+++ c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program6.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 void isperfect();
 void main()
@@ -24,31 +23,4 @@ void isperfect(){
     else
         printf("Not Perfect");
 }
-=======
-#include <stdio.h>
-void isperfect();
-void main()
-{
-	 isperfect();
-}
-void isperfect(){
-    int n = 28;
-    int i = 1;
-    int sum = 0;
-
-    while (i <= n / 2)
-    {
-        if (n % i == 0)
-        {
-            sum = sum + i;
-        }
-        i++;
-    }
-
-    if (sum == n)
-        printf("Perfect");
-    else
-        printf("Not Perfect");
-}
->>>>>>> a6e0a47394198f786ebdc5e4b1e1d7f42da419ee
 // This program checks if a hardcoded number is a perfect number or not
