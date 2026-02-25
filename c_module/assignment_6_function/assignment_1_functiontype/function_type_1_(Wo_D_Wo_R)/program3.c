322d9d2d55e6093ca9ee7ee8c2770016b2e88eae
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 19:49:36 +0530

    Remove leap year check implementation

--- c_module/assignment_6_function/assignment_1_functiontype/function_type_1_(Wo_D_Wo_R)/program3.c
+++ c_module/assignment_6_function/assignment_1_functiontype/function_type_1_(Wo_D_Wo_R)/program3.c
@@ -21,27 +21,3 @@ void checkLeap()    // function definition
         printf("Not Leap year");
     }
 }
-=======
-#include<stdio.h>
-
-void checkLeap();   // function declaration
-
-void main()
-{
-    checkLeap();    // function call
-}
-
-void checkLeap()    // function definition
-{
-    int year = 2059;
-
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-    {
-        printf("Leap year");
-    }
-    else
-    {
-        printf("Not Leap year");
-    }
-}
-
