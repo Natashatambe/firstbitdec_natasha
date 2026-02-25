ba4929eef0de7dd5ea1b6b49b0e71634f7c1826b
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 22:53:21 +0530

    Refactor program7.c to call isfactor function

--- c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program7.c
+++ c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program7.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 void isfactor();
 void main()
@@ -17,23 +16,3 @@ void isfactor(){
 
     printf("%d", fact);
 }
-=======
-#include <stdio.h>
-void isfactor();
-void main()
-{
-     isfactor();
-}
-void isfactor(){
-    int n = 5;
-    int fact = 1;
-
-    while (n > 0)
-    {
-        fact = fact * n;
-        n--;
-    }
-
-    printf("%d", fact);
-}
->>>>>>> a6e0a47394198f786ebdc5e4b1e1d7f42da419ee
