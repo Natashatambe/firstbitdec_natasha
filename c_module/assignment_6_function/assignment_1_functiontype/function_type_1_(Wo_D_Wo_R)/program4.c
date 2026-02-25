44ae14676a91dd70c3930b5a09d151644821f43b
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 19:48:53 +0530

    Remove checkCharacter function and its invocation
    Removed the checkCharacter function and its call from main.

--- c_module/assignment_6_function/assignment_1_functiontype/function_type_1_(Wo_D_Wo_R)/program4.c
+++ c_module/assignment_6_function/assignment_1_functiontype/function_type_1_(Wo_D_Wo_R)/program4.c
@@ -22,28 +22,5 @@ void checkCharacter()    // no parameter, no return type
         printf("Given character is consonant");
     }
 }
-=======
-#include<stdio.h>
 
-void checkCharacter();   // function declaration
-
-void main()
-{
-    checkCharacter();    // function call
-}
-
-void checkCharacter()    // no parameter, no return type
-{
-    char ch = 'p';   // hardcoded value
-
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
-       ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-    {
-        printf("Given character is vowel");
-    }
-    else
-    {
-        printf("Given character is consonant");
-    }
-}
 
