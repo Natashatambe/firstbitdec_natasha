ec15babf287715ba7f0bdd57a2f34a90fd7c2a9b
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 13:34:28 +0530

    Refactor prime number program structure
    Refactor prime number checking logic for clarity.

--- c_module/assignment_4_nested_loop/program2.c
+++ c_module/assignment_4_nested_loop/program2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 void main() {
-    int n = 50;  // You can change this value
-    int num = 2; // Start from 2, since 1 is not prime
+    int n = 50;  
+    int num = 2; 
 
     printf("Prime numbers from 1 to %d are:\n", n);
 
     while (num <= n) {
         int i = 2;
-        int isPrime = 1; // Assume number is prime
+        int isPrime = 1; 
 
         // Check if num is divisible by any number from 2 to num-1
-        while (i < num) {  // Nested loop
+        while (i < num) {  
             if (num % i == 0) {
                 isPrime = 0; // Not prime
             }
