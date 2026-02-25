37e02c5872193881068708ff6d53b20aff7ce93f
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 19:50:26 +0530

    Refactor salary calculation into salaryCal function
    Refactored salary calculation logic into a separate function and updated main function to call it.

--- c_module/assignment_6_function/assignment_1_functiontype/function_type_1_(Wo_D_Wo_R)/program6.c
+++ c_module/assignment_6_function/assignment_1_functiontype/function_type_1_(Wo_D_Wo_R)/program6.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<stdio.h>
 void salaryCal();
 int main()
@@ -24,31 +24,5 @@ int main()
 	printf("TOTAL SALARY :%d", TOTALsalary);
 }
 // This program calculates the total salary based on a hardcoded basic salary
-=======
-#include<stdio.h>
-void salaryCal();
-int main()
-{
-	salaryCal();
-}
- void salaryCal()
- {
- 	
-	int Basic=1200;
-	int DA,TA,HRA,TOTALsalary;
-	if(Basic<=5000){
-		DA =(10*Basic)/100;
-		TA =(20*Basic)/100;
-		HRA =(25*Basic)/100;
-	}
-	else{
-		DA =(15*Basic)/100;
-		TA =(25*Basic)/100;
-		HRA =(30*Basic)/100;
-	}
-	TOTALsalary = Basic + DA + TA + HRA;
-	printf("TOTAL SALARY :%d", TOTALsalary);
-}
-// This program calculates the total salary based on a hardcoded basic salary
->>>>>>> a6e0a47394198f786ebdc5e4b1e1d7f42da419ee
+
 // and different allowances depending on the salary range.
