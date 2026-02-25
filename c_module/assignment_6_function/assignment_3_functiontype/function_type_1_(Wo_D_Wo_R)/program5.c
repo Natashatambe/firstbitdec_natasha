517cfc953a80c36e809a003fcf9944aaf4db0a7a
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 22:52:25 +0530

    Refactor Armstrong number check program
    Removed duplicate main function and isamstrong function implementation. Simplified the program to check if a hardcoded number is an Armstrong number.

--- c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program5.c
+++ c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program5.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 void isamstrong();
 int main()
@@ -25,32 +24,4 @@ void isamstrong(){
 			printf("not amstrong number");
 		}
 }
-=======
-#include<stdio.h>
-void isamstrong();
-int main()
-{
-	isamstrong();
-	
-}
-void isamstrong(){
-	int i=1,n=7,count=0;
-	while(i<n)
-	{
-		if(n%i==0)
-		{
-			count=1;
-		}
-		i++;
-	}
-		if(count == 0 && n>1)
-		{
-			printf("amstrong number");
-		}
-		else
-		{
-			printf("not amstrong number");
-		}
-}
->>>>>>> a6e0a47394198f786ebdc5e4b1e1d7f42da419ee
 // This program checks if a hardcoded number is an Armstrong number or not
