db3361a4964c668cc2e871ad5a5db5428797d474
natasha <108368242+Natashatambe@users.noreply.github.com>
2026-02-25 22:51:17 +0530

    Remove redundant main function and clean up code

--- c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program3.c
+++ c_module/assignment_6_function/assignment_3_functiontype/function_type_1_(Wo_D_Wo_R)/program3.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 
 void sum();
@@ -19,25 +18,3 @@ void sum()
     }
     printf("%d", sum);
 }
-=======
-#include<stdio.h>
-
-void sum();
-
-int main()
-{
-    sum();   // function call
-    return 0;
-}
-
-void sum()
-{
-    int start=1, end=5, sum=0;
-    while(start <= end)
-    {
-        sum = sum + start;
-        start++;
-    }
-    printf("%d", sum);
-}
->>>>>>> a6e0a47394198f786ebdc5e4b1e1d7f42da419ee
